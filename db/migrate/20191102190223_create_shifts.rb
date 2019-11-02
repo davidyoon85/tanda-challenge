6d8d778fe90702445031d7a2ff2fa075919ce5c6
David Yoon <davidyoon85@gmail.com>
2019-11-02 15:18:08 -0400

    create models

---
+++ db/migrate/20191102190223_create_shifts.rb
@@ -0,0 +1,12 @@
+class CreateShifts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shifts do |t|
+      t.integer :user_id, null: false
+      t.datetime :start, null: false
+      t.datetime :finish, null: false
+      t.integer :break_length
+
+      t.timestamps
+    end
+  end
+end
