3157d15565e98851b0a22bfd2e0dc036609a96b0
David Yoon <davidyoon85@gmail.com>
2019-11-04 15:20:24 -0500

    forgot password functionality

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'david.tanda.challenge@gmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,21 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'david.tanda.challenge@gmail.com',
+    password: '!tanda123',
+    authentication: 'plain',
+   }
 end
